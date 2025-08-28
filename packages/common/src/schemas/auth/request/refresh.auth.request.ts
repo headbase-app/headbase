85dc9091f6d1d1d09812d9ce99cbfbf2ed5746d3
Ben Ryder <dev@benryder.me>
2025-08-28 15:07:43 +0100

    feat: package reorganise

--- packages/common/src/schemas/auth/request/refresh.auth.request.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod";
-import isJWT from "validator/lib/isJWT";
-
-export const RefreshRequest = z.object({
-    refreshToken: z.string()
-      .refine(isJWT, {message: "refreshToken must be a JWT"})
-}).strict();
-
-export type RefreshRequest = z.infer<typeof RefreshRequest>;
--- packages/common/src/schemas/auth/response/auth-user-response.ts
+++
@@ -1,7 +0,0 @@
-import {TokenPair} from "../tokens";
-import {UserDto} from "../../users/users";
-
-export interface AuthUserResponse {
-  tokens: TokenPair;
-  user: UserDto;
-}
